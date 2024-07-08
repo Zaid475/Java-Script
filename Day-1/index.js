9ce2e93e6cd98f4166e646a14b19fba9f24c24df
Zaid475 <54655785+Zaid475@users.noreply.github.com>
2024-07-08 16:22:09 +0530

    day 1 updates

---
+++ Day-1/index.js
@@ -0,0 +1,77 @@
+var nam="zaid"; 
+var name1='diaz';
+var name2=`cr7`;
+
+console.log(nam);
+console.log(name1);
+console.log(name2);
+console.log(nam,name1,name2);
+console.log(typeof(nam));
+console.log(typeof(name1));
+console.log(typeof(name2));
+
+var number=7;
+var number1=8;
+console.log(number);
+console.log(number1);
+console.log(number + number1);
+console.log(number - number1);
+console.log(number * number1);
+console.log(number / number1);
+
+
+
+
+var boolean=true;
+var boolean1=false;
+console.log(boolean);
+console.log(boolean1);
+console.log(typeof(boolean));
+console.log(typeof(boolean1));
+
+var age;
+console.log(age);
+console.log(typeof(age));
+
+{var user="zaid";
+console.log(user);
+user="abcd";
+console.log(user);
+var user="cris";
+console.log(user);
+}
+console.log(user);
+console.log(user);
+console.log(user);
+
+
+{let username="ramesh";
+console.log(username);
+username="keval";
+console.log(username);
+// let username="jay";
+// console.log(username);
+}
+// console.log(username);
+
+
+{const username1="khali";
+console.log(username1);
+// username1="great khali";
+// console.log(username1);
+// const username1="zaid";
+// console.log(username1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
