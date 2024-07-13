eceb7afddf6e43ab50991290090976b872ef394b
Zaid475 <54655785+Zaid475@users.noreply.github.com>
2024-07-13 13:08:52 +0530

    day 4 updates

---
+++ Day 4/index.js
@@ -0,0 +1,24 @@
+function validageforlicense(age){
+    if(age>18 && age<=60){
+        console.log("User is eligible for driving license",age);
+    }
+    else if(age<=17 || age>60){
+        console.log("User is not eligible for driving license",age);
+
+    }
+    else {
+        console.log("User is eliglible for learning license",age);
+    }
+    
+
+
+}
+validageforlicense(18)
+
+// Q. Check is user valid for DL, on basis of below conditions,
+
+// Tip - use logiccal operator to put two condition at same Time .
+
+// 1. if -> 2 condition -> User age must be greater than 18 and lesser than and equal 60. -> eligible for DL. 
+// 2. else if -> 2 condition -> equal to and lesser than 17 or greater than 60. -> not eligible for DL
+// 3. else . equal to 18 -> eligible for LL.
