483b0f82c195982ecad513724efd693ac7c46ca3
Zaid475 <54655785+Zaid475@users.noreply.github.com>
2024-07-22 21:47:06 +0530

    day 9 updates

---
+++ Day 9/index.js
@@ -0,0 +1,65 @@
+// Q Given an array of names find out index of target . 
+// var students = ["rahul", "nehal", "ali", "amod", "adep"];
+// var target = "adep"
+// output  : 4
+
+
+// function targetfinder(array){
+
+// var target = "adep"
+// for(i=0; i<=array.length-1; i++){
+// if(array[i]==target){
+//     return i;
+// }
+
+// }
+
+
+// }
+// var students = ["rahul", "nehal", "ali", "amod", "adep"];
+// console.log(targetfinder(students));
+
+
+//push
+
+var data=["zaid","cristiano","messi","neymar","bellingham","mbappe"];
+// console.log(data);
+// data.push("modric");
+// console.log(data);
+// data.push("rodrygo");
+// console.log(data);
+
+// //pop
+// data.pop();
+// console.log(data);
+
+// data.pop();
+// console.log(data);
+
+//shift
+// data.shift();
+// console.log(data);
+
+// unshift
+// data.unshift("zaid");
+// console.log(data);
+
+//slice
+// var data=["zaid","cristiano","messi","neymar","bellingham","mbappe"];
+
+// console.log(data.slice(0,3));
+// console.log(data.slice(0));
+// console.log(data.slice(2,-1));
+// console.log(data.slice(-4,-1));
+
+
+//splice
+// var data=["zaid","cristiano","messi","neymar","bellingham","mbappe"];
+// data.splice(2,0,"ramos");
+// console.log(data);
+
+
+
+// data.splice(4,1,"rodri");
+// console.log(data);
+
